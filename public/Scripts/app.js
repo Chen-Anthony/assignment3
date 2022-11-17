c4c08cc197e033c7a37959a8b4b9367ade21b99e
Chen-Anthony <jia.chen4@ontariotechu.net>
2022-11-17 15:40:55 -0500

    creating controller to organize files

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -2,7 +2,18 @@
     function Start()
     {
         console.log("App started")
-       
+        let deleteButtons=document.querySelectorAll('.btn-danger');
+        for(button of deleteButtons){
+            button.addEventListener('click',(event)=>{
+                if(!confirm("are you sure"))
+                {
+                    event.preventDefault();
+                    window.location.assign('/book-list');
+                }
+            });
+        }
     }
     window.addEventListener("load",Start);
+    
+    
 })();
