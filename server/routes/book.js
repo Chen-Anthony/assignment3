171ee5583a595294abfc150f597f9d96ac26639b
Chen-Anthony <jia.chen4@ontariotechu.net>
2022-11-17 03:51:47 -0500

    fix errors

--- server/routes/book.js
+++ server/routes/book.js
@@ -9,13 +9,90 @@ router.get('/',(req,res,next)=>{
             return console.error(err);
         }
         else{
-            res.render('book',{
-            title:'Book List',
+            res.render('book/list',{
+            title:'Books',
             Booklist: Booklist})
         }
     });
     
     
 });
+router.get('/add',(req,res,next)=>{
+    res.render('book/add',{title:'Add book'})
+
+});
+router.post('/add',(req,res,next)=>{
+    let newBook=Book({
+        "name":req.body.name,
+        "author":req.body.author,
+        "published":req.body.published,
+        "description":req.body.description,
+        "price":req.body.price
+    });
+    Book.create(newBook,(err,Book)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect('/book-list');
+        }
+    })
+
+});
+router.get('/edit/:id',(req,res,next)=>{
+    let id=req.params.id;
+    Book.findById(id,(err,bookToEdit)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.render('book/edit',{title:"Edit Book",book:bookToEdit});
+        }
+    });
+
+});
+router.post('/edit/:id',(req,res,next)=>{
+    let id=req.params.id;
+    let updateBook=Book({
+        "name":req.body.name,
+        "author":req.body.author,
+        "published":req.body.published,
+        "description":req.body.description,
+        "price":req.body.price
+    });
+    Book.updateOne({_id:id},updateBook,(err)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect('/book-list');
+        }
+    });
+
+});
+router.get('/delete/:id',(req,res,next)=>{
+    let id=req.params.id;
+    Book.deleteOne({_id:id},(err)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect('/book-list');
+        
+        }
+    });
+
+});
+
+
+
 module.exports=router;
 
